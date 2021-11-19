27c559ee871b63f424128f1f2720c6cccaf53561
Alberto Reyer <alberto.reyer@spryker.com>
2021-11-19 10:35:47 +0100

    - implement dynamic task loading for tasks that are defined as services - implement task with type php (essentially any service can be exposed as task if it implements the right interface)

---
+++ src/SprykerSdk/Sdk/Extension/Tasks/HelloPhpCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerSdk\Sdk\Extension\Tasks;
+
+use SprykerSdk\Sdk\Core\Domain\Entity\ExecutableCommandInterface;
+
+class HelloPhpCommand implements ExecutableCommandInterface
+{
+    /**
+     * @return string
+     */
+    public function getCommand(): string
+    {
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return 'php';
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasStopOnError(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @param array $resolvedValues
+     *
+     * @return int
+     */
+    public function execute(array $resolvedValues): int
+    {
+        echo "Hello PHP";
+
+        return 0;
+    }
+}
