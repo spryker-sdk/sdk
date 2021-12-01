35a268f6af2cd8bf7b385e68dcad6937cd7debc7
Kyrylo Meretskyi <kyrylo.meretskyi@spryker.com>
2021-12-01 16:19:40 +0200

    SDK-80: fixed licence

--- src/App/Migrations/Version20211201132057.php
+++ src/App/Migrations/Version20211201132057.php
@@ -1,15 +1,11 @@
 <?php
-/*
- * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
- * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
- */
-
-declare(strict_types=1);
 
 /**
  * Copyright © 2019-present Spryker Systems GmbH. All rights reserved.
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
+
+declare(strict_types=1);
 
 namespace App\Migrations;
 
