7b113e2e928358c3e9673426c2831e3be24cece9
Alberto Reyer <alberto.reyer@spryker.com>
2021-12-09 14:19:03 +0100

    POC packaging & distribution:
    - use sqlite based migration instead of postgres based one (using sequence vs. auto increment)

---
+++ app/Migrations/Version20211117132317.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright © 2019-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211117132317 extends AbstractMigration
+{
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     *
+     * @return void
+     */
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE sdk_setting (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, path VARCHAR(255) NOT NULL, "values" CLOB DEFAULT NULL --(DC2Type:json)
+        , strategy VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, is_project BOOLEAN NOT NULL, has_initialization BOOLEAN NOT NULL, initialization_description VARCHAR(255) DEFAULT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8463B8E1B548B0F ON sdk_setting (path)');
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     *
+     * @return void
+     */
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE sdk_setting');
+    }
+}
