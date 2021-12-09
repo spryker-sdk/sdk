7e776241558d876da861ad10816602a63ea129d4
Kyrylo Meretskyi <kyrylo.meretskyi@spryker.com>
2021-12-09 12:27:32 +0200

    SDK-80: fixed File relation to RemovedEvent

--- src/Infrastructure/Entity/File.php
+++ src/Infrastructure/Entity/File.php
@@ -13,28 +13,6 @@ class File extends EntityFile
 {
     protected int $id;
 
-    protected RemovedEvent $removedEvent;
-
-    /**
-     * @return \SprykerSdk\Sdk\Infrastructure\Entity\RemovedEvent
-     */
-    public function getRemovedEvent(): RemovedEvent
-    {
-        return $this->removedEvent;
-    }
-
-    /**
-     * @param \SprykerSdk\Sdk\Infrastructure\Entity\RemovedEvent $removedEvent
-     *
-     * @return $this
-     */
-    public function setRemovedEvent(RemovedEvent $removedEvent)
-    {
-        $this->removedEvent = $removedEvent;
-
-        return $this;
-    }
-
     /**
      * @return int
      */
@@ -51,30 +29,6 @@ class File extends EntityFile
     public function setId(int $id)
     {
         $this->id = $id;
-
-        return $this;
-    }
-
-    /**
-     * @param string $path
-     *
-     * @return $this
-     */
-    public function setPath(string $path)
-    {
-        $this->path = $path;
-
-        return $this;
-    }
-
-    /**
-     * @param string $content
-     *
-     * @return $this
-     */
-    public function setContent(string $content)
-    {
-        $this->content = $content;
 
         return $this;
     }
