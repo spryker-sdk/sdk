988237474dc1b74e955a3496f4c3585f5097208e
Alberto Reyer <alberto.reyer@spryker.com>
2021-12-13 12:46:27 +0100

    POC packaging & distribution:
    - resolve todo's

--- infrastructure/create_release.sh
+++ infrastructure/create_release.sh
@@ -24,6 +24,10 @@ cat "${BUILD_DIR}/spryker-sdk.tar.gz" >> "${BUILD_DIR}/installer.sh"
 chmod a+x "${BUILD_DIR}/installer.sh"
 
 docker-compose -f docker-compose.yml build --no-cache
-#@todo push to docker hub
-#@todo create git tag
-#@todo push installer to github
+
+echo "Nearly done, the next steps are:"
+echo "docker login"
+echo "docker push spryker-sdk:${VERSION}"
+echo "git tag ${VERSION} && git push git push origin ${VERSION}"
+echo "create a github release (https://docs.github.com/en/repositories/releasing-projects-on-github/managing-releases-in-a-repository#creating-a-release)"
+echo "upload the ${BUILD_DIR}/installer.sh to the github release"
