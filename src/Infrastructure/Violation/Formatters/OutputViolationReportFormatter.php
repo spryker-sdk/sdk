392fc78fca3ad4d729ac342d221c4ded563b7629
vol4onok <alexander.veselov@spryker.com>
2021-12-16 19:49:25 +0200

    Created factory

---
+++ src/Infrastructure/Violation/Formatters/OutputViolationReportFormatter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SprykerSdk\Sdk\Infrastructure\Violation\Formatters;
+
+use SprykerSdk\Sdk\Core\Appplication\Violation\ViolationReportFormatterInterface;
+use SprykerSdk\SdkContracts\Violation\ViolationReportInterface;
+
+class OutputViolationReportFormatter implements ViolationReportFormatterInterface
+{
+    /**
+     * @param \SprykerSdk\SdkContracts\Violation\ViolationReportInterface $violationReport
+     *
+     * @return mixed
+     */
+    public function format(ViolationReportInterface $violationReport): mixed
+    {
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormat(): string
+    {
+
+    }
+}
