af5c8b5224a7d17a2827a482416c4cd06a98e331
Kyrylo Meretskyi <kyrylo.meretskyi@spryker.com>
2021-12-23 10:16:53 +0200

    SDK-258: fixed licences

--- tests/_support/Helper/Core/Application/Service/ContextSerializerHelper.php
+++ tests/_support/Helper/Core/Application/Service/ContextSerializerHelper.php
@@ -1,10 +1,5 @@
 <?php
 
-/*
- * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
- * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
- */
-
 /**
  * Copyright © 2019-present Spryker Systems GmbH. All rights reserved.
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
