6e865f3608488a0f47efb01a082209f7ba2fee40
Kyrylo Meretskyi <kyrylo.meretskyi@spryker.com>
2021-12-23 14:27:51 +0200

    SDK-258: fixes

--- tests/Sdk/Tests/Core/Application/Lifecycle/Event/InitializedEventTest.php
+++ tests/Sdk/Tests/Core/Application/Lifecycle/Event/InitializedEventTest.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace  SprykerSdk\Sdk\Tests\Core\Application\Lifecycle\Event;
+namespace SprykerSdk\Sdk\Tests\Core\Application\Lifecycle\Event;
 
 use Codeception\Test\Unit;
 use SprykerSdk\Sdk\Core\Appplication\Lifecycle\Event\InitializedEvent;
