07656af0817ff2ce15274230d71369700e5fcf24
Artem Miroshnikov <94458630+miroshnikov-spryker@users.noreply.github.com>
2022-01-27 16:00:58 +0300

    SDK-332 SCSS linter integration (#66)

---
+++ src/Extension/Converters/ScssLinterViolationReportConverter.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * Copyright © 2019-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerSdk\Sdk\Extension\Converters;
+
+use SprykerSdk\Sdk\Core\Appplication\Violation\AbstractViolationConverter;
+use SprykerSdk\Sdk\Core\Domain\Entity\Violation\PackageViolationReport;
+use SprykerSdk\Sdk\Core\Domain\Entity\Violation\ViolationReport;
+use SprykerSdk\Sdk\Core\Domain\Entity\Violation\ViolationReportConverter;
+use SprykerSdk\SdkContracts\Violation\ViolationReportInterface;
+
+class ScssLinterViolationReportConverter extends AbstractViolationConverter
+{
+    /**
+     * @var string
+     */
+    protected string $fileName;
+
+    /**
+     * @var string
+     */
+    protected string $producer;
+
+    /**
+     * @param array $configuration
+     *
+     * @return void
+     */
+    public function configure(array $configuration): void
+    {
+        $this->fileName = $configuration['input_file'];
+        $this->producer = $configuration['producer'];
+    }
+
+    /**
+     * @return \SprykerSdk\SdkContracts\Violation\ViolationReportInterface|null
+     */
+    public function convert(): ?ViolationReportInterface
+    {
+        $projectDirectory = $this->settingRepository->findOneByPath('project_dir');
+        if (!$projectDirectory) {
+            return null;
+        }
+
+        $jsonReport = $this->readFile();
+        if (!$jsonReport) {
+            return null;
+        }
+
+        $report = json_decode($jsonReport, true);
+        $report = array_filter((array)$report, fn (array $file): bool => !empty($file['errored']));
+        if (empty($report)) {
+            return null;
+        }
+
+        return new ViolationReport(
+            basename($projectDirectory->getValues()),
+            '.' . DIRECTORY_SEPARATOR,
+            [],
+            $this->getPackages($projectDirectory->getValues(), $report),
+        );
+    }
+
+    /**
+     * @param string $projectDirectory
+     * @param array $files
+     *
+     * @return array<\SprykerSdk\SdkContracts\Violation\PackageViolationReportInterface>
+     */
+    protected function getPackages(string $projectDirectory, array $files): array
+    {
+        $packages = [];
+        foreach ($files as $file) {
+            if (!$file['warnings']) {
+                continue;
+            }
+
+            $relatedPathToFile = ltrim(str_replace($projectDirectory, '', $file['source']), DIRECTORY_SEPARATOR);
+            $moduleName = $this->resolveModuleName($relatedPathToFile);
+            $pathToModule = $this->resolvePathToModule($relatedPathToFile);
+
+            $fileViolations = [];
+            foreach ($file['warnings'] as $warning) {
+                $fileViolations[$relatedPathToFile][] = new ViolationReportConverter(
+                    basename($relatedPathToFile, '.scss'),
+                    $warning['text'],
+                    null,
+                    null,
+                    (int)$warning['line'],
+                    (int)$warning['line'],
+                    (int)$warning['column'],
+                    (int)$warning['column'],
+                    null,
+                    $warning,
+                    false,
+                    $this->producer,
+                );
+            }
+
+            $packages[] = new PackageViolationReport(
+                $moduleName,
+                $pathToModule,
+                [],
+                $fileViolations,
+            );
+        }
+
+        return $packages;
+    }
+}
