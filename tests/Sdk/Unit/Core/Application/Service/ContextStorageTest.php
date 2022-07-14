25de14ada26588cdee5f2c08c9189d1567d57913
Sergey Romankov <sergey.romankov@spryker.com>
2022-07-14 23:18:11 +0300

    SDK-691 add application service tests

---
+++ tests/Sdk/Unit/Core/Application/Service/ContextStorageTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Copyright © 2019-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Sdk\Unit\Core\Application\Service;
+
+use Codeception\Test\Unit;
+use LogicException;
+use SprykerSdk\Sdk\Core\Appplication\Service\ContextStorage;
+use SprykerSdk\SdkContracts\Entity\ContextInterface;
+
+/**
+ * @group Sdk
+ * @group Core
+ * @group Application
+ * @group Service
+ * @group ContextStorageTest
+ */
+class ContextStorageTest extends Unit
+{
+    /**
+     * @return void
+     */
+    public function testThrowExceptionWhenContextIsNotSet(): void
+    {
+        //Arrange
+        $storage = new ContextStorage();
+
+        //Act
+        $this->expectException(LogicException::class);
+        $storage->getContext();
+    }
+
+    /**
+     * @return void
+     */
+    public function testReturnsContextWhenContextIsSet(): void
+    {
+        //Arrange
+        $storage = new ContextStorage();
+        $context = $this->createMock(ContextInterface::class);
+
+        //Act
+        $storage->setContext($context);
+        $foundContext = $storage->getContext();
+
+        //Assert
+        $this->assertSame($context, $foundContext);
+    }
+}
---
+++ tests/Sdk/Unit/Core/Application/Service/ConverterResolverTest.php
@@ -0,0 +1,149 @@
+<?php
+
+/**
+ * Copyright © 2019-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Sdk\Unit\Core\Application\Service;
+
+use Codeception\Test\Unit;
+use SprykerSdk\Sdk\Core\Appplication\Dependency\ConverterRegistryInterface;
+use SprykerSdk\Sdk\Core\Appplication\Service\ConverterResolver;
+use SprykerSdk\SdkContracts\Entity\CommandInterface;
+use SprykerSdk\SdkContracts\Entity\ConverterInterface;
+use SprykerSdk\SdkContracts\Report\ReportConverterInterface;
+
+/**
+ * @group Sdk
+ * @group Core
+ * @group Application
+ * @group Service
+ * @group ConverterResolverTest
+ */
+class ConverterResolverTest extends Unit
+{
+    /**
+     * @return void
+     */
+    public function testReturnsNullWhenCommandConverterIsNull(): void
+    {
+        //Arrange
+        $command = $this->createCommandMock();
+        $converterRegistry = $this->createConverterRegistryMock();
+        $converterResolver = new ConverterResolver($converterRegistry);
+
+        //Act
+        $reportConverter = $converterResolver->resolve($command);
+
+        //Assert
+        $this->assertNull($reportConverter);
+    }
+
+    /**
+     * @return void
+     */
+    public function testReturnsNullWhenRegistryHasNoReportConverter(): void
+    {
+        //Arrange
+        $converter = $this->createConverterMock('test');
+        $command = $this->createCommandMock($converter);
+        $converterRegistry = $this->createConverterRegistryMock();
+        $converterRegistry->method('has')->willReturn(false);
+
+        $converterResolver = new ConverterResolver($converterRegistry);
+
+        //Act
+        $reportConverter = $converterResolver->resolve($command);
+
+        //Assert
+        $this->assertNull($reportConverter);
+    }
+
+    /**
+     * @return void
+     */
+    public function testReturnsNullWhenRegistryHasNullConverter(): void
+    {
+        //Arrange
+        $converter = $this->createConverterMock('test');
+        $command = $this->createCommandMock($converter);
+
+        $converterRegistry = $this->createConverterRegistryMock();
+        $converterRegistry->method('has')->willReturn(true);
+        $converterRegistry->method('get')->willReturn(null);
+
+        $converterResolver = new ConverterResolver($converterRegistry);
+
+        //Act
+        $reportConverter = $converterResolver->resolve($command);
+
+        //Assert
+        $this->assertNull($reportConverter);
+    }
+
+    /**
+     * @return void
+     */
+    public function testReturnsReportConverterWhenItExists(): void
+    {
+        //Arrange
+        $converter = $this->createConverterMock('test');
+        $command = $this->createCommandMock($converter);
+        $reportConverter = $this->createReportConverterMock();
+
+        $converterRegistry = $this->createConverterRegistryMock();
+        $converterRegistry->method('has')->willReturn(true);
+        $converterRegistry->method('get')->willReturn($reportConverter);
+
+        $converterResolver = new ConverterResolver($converterRegistry);
+
+        //Act
+        $returnedReportConverter = $converterResolver->resolve($command);
+
+        //Assert
+        $this->assertSame($reportConverter, $returnedReportConverter);
+    }
+
+    /**
+     * @return \SprykerSdk\Sdk\Core\Appplication\Dependency\ConverterRegistryInterface|\Sdk\Unit\Core\Application\Service\MockObject
+     */
+    protected function createConverterRegistryMock(): ConverterRegistryInterface
+    {
+        return $this->createMock(ConverterRegistryInterface::class);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return \SprykerSdk\SdkContracts\Entity\ConverterInterface
+     */
+    protected function createConverterMock(string $name): ConverterInterface
+    {
+        $converterMock = $this->createMock(ConverterInterface::class);
+        $converterMock->method('getName')->willReturn($name);
+
+        return $converterMock;
+    }
+
+    /**
+     * @param \SprykerSdk\SdkContracts\Entity\ConverterInterface|null $converter
+     *
+     * @return \SprykerSdk\SdkContracts\Entity\CommandInterface
+     */
+    protected function createCommandMock(?ConverterInterface $converter = null): CommandInterface
+    {
+        $commandMock = $this->createMock(CommandInterface::class);
+        $commandMock->method('getConverter')->willReturn($converter);
+
+        return $commandMock;
+    }
+
+    /**
+     * @return \SprykerSdk\SdkContracts\Report\ReportConverterInterface
+     */
+    protected function createReportConverterMock(): ReportConverterInterface
+    {
+        return $this->createMock(ReportConverterInterface::class);
+    }
+}
