44292de492e8f6dc54b2ee1eb2c7f0f9c4af5dab
vol4onok <alexander.veselov@spryker.com>
2022-07-18 16:21:15 +0300

    SDK-691: Created tests for value resolvers

--- src/Extension/ValueResolvers/StaticValueResolver.php
+++ src/Extension/ValueResolvers/StaticValueResolver.php
@@ -81,6 +81,14 @@ class StaticValueResolver extends AbstractValueResolver implements ConfigurableV
     }
 
     /**
+     * @return string
+     */
+    public function getHelp(): ?string
+    {
+        return $this->help;
+    }
+
+    /**
      * @return string|null
      */
     public function getAlias(): ?string
