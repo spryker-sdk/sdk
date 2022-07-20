bf93d5ac27929ab532ca6a1cd6eb20e07b02fae7
vol4onok <alexander.veselov@spryker.com>
2022-07-20 10:39:58 +0300

    SDK-691: Updated Dto test

--- tests/Sdk/Unit/Core/Application/Dto/Abstraction/DtoTest.php
+++ tests/Sdk/Unit/Core/Application/Dto/Abstraction/DtoTest.php
@@ -25,6 +25,119 @@ class DtoTest extends Unit
     /**
      * @return void
      */
+    public function testDtoCreate(): void
+    {
+        // Arrange
+        $dto = new class extends Dto {
+            /**
+             * @var int
+             */
+            protected int $integer;
+
+            /**
+             * @var float
+             */
+            protected float $double;
+
+            /**
+             * @var bool
+             */
+            protected bool $boolean;
+
+            /**
+             * @var mixed
+             */
+            protected mixed $anything;
+
+            /**
+             * @var array<string, string>
+             */
+            protected array $arr;
+
+            /**
+             * @var self|null
+             */
+            protected ?self $dto;
+
+            /**
+             * @return int
+             */
+            public function getInteger(): int
+            {
+                return $this->integer;
+            }
+
+            /**
+             * @return float
+             */
+            public function getDouble(): float
+            {
+                return $this->double;
+            }
+
+            /**
+             * @return bool
+             */
+            public function isBoolean(): bool
+            {
+                return $this->boolean;
+            }
+
+            /**
+             * @return mixed
+             */
+            public function getAnything(): mixed
+            {
+                return $this->anything;
+            }
+
+            /**
+             * @return array<string, string>
+             */
+            public function getArr(): array
+            {
+                return $this->arr;
+            }
+
+            /**
+             * @return self|null
+             */
+            public function getDto(): ?self
+            {
+                return $this->dto;
+            }
+        };
+        $dtoData = [
+            'integer' => 1,
+            'double' => 1.0,
+            'boolean' => true,
+            'anything' => new stdClass(),
+            'arr' => ['key' => 'value', 'key1' => 'value1'],
+            'dto' => [
+                'integer' => 2,
+                'double' => 2.0,
+                'boolean' => true,
+                'anything' => new stdClass(),
+                'arr' => ['key2' => 'value2', 'key3' => 'value3'],
+                'dto' => null,
+            ],
+        ];
+
+        // Act
+        $dto = $dto::create($dtoData);
+
+        // Assert
+        $this->assertSame($dtoData['integer'], $dto->getInteger());
+        $this->assertSame($dtoData['double'], $dto->getDouble());
+        $this->assertSame($dtoData['boolean'], $dto->isBoolean());
+        $this->assertSame($dtoData['anything'], $dto->getAnything());
+        $this->assertSame($dtoData['arr'], $dto->getArr());
+        $this->assertInstanceOf(Dto::class, $dto->getDto());
+    }
+
+    /**
+     * @return void
+     */
     public function testDtoCreationFromArray(): void
     {
         // Arrange
