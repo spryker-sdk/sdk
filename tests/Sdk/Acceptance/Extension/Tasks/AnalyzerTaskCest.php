1cbb577ac4e1722f004dac2e8f6ec6f0da27ddc4
Kyrylo Meretskyi <kyrylo.meretskyi@spryker.com>
2022-07-26 14:31:42 +0300

    SDK-657: fixed directories for tests

--- tests/Sdk/Acceptance/Extension/Tasks/AnalyzerTaskCest.php
+++ tests/Sdk/Acceptance/Extension/Tasks/AnalyzerTaskCest.php
@@ -73,7 +73,7 @@ class AnalyzerTaskCest
 
         // Assert
         Assert::assertFileExists(
-            $I->getPathFromProjectRoot('.ssdk/reports/' . static::COMMAND . '.violations.yaml', static::FAIL_PROJECT_DIR),
+            $I->getPathFromProjectRoot('reports/' . static::COMMAND . '.violations.yaml', static::FAIL_PROJECT_DIR),
         );
     }
 }
