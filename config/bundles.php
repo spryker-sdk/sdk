5200062f193fb2f21c754f307ebc4212600a85bc
Sergey Romankov <sergey.romankov@spryker.com>
2022-08-09 17:04:04 +0300

    SDK-809 add logger

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Upgrader\UpgraderBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
