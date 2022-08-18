cc2265df106c383d8f2d23f824ea7cad867a3198
pavlo.maksymov <pavlo.maksymov@spryker.com>
2022-08-18 16:35:17 +0200

    APPS-4764: Fixed tests.

--- tests/_project/project/src/CodeSniffer/success/Success.php
+++ tests/_project/project/src/CodeSniffer/success/Success.php
@@ -1,24 +1,29 @@
 <?php
 
 /**
- * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
- * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ * Success file.
+ * php version 7.4.0
+ *
+ * @category Stub
+ * @package  SprykerSdk
+ * @author   Oryx <oryx@spryker.com>
+ * @license  See LICENSE file.
+ * @version  GIT: @Release: 0.3.4@
+ * @link     https://spryker.com/
  */
 
 namespace SprykerSdk\Sdk\Acceptance\Extension\Tasks\CodeSniffer;
 
+/**
+ * Success file.
+ *
+ * @category Stub
+ * @package  SprykerSdk
+ * @author   Oryx <oryx@spryker.com>
+ * @license  See LICENSE file.
+ * @version  GIT: @Release: 0.3.4@
+ * @link     https://spryker.com/
+ */
 class Success
 {
-    /**
-     * @var array<string>
-     */
-    protected array $tests;
-
-    /**
-     * @param array<string> $tests
-     */
-    public function __construct(array $tests)
-    {
-        $this->tests = $tests;
-    }
 }
