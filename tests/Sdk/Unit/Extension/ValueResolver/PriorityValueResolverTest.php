9e7fdc64e1eddd4a03451a1401cee87e9d01f690
vol4onok <alexander.veselov@spryker.com>
2022-09-19 14:39:40 +0300

    SDK-869: SDK-905: Created tests.

---
+++ tests/Sdk/Unit/Extension/ValueResolver/PriorityValueResolverTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Copyright © 2019-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerSdk\Sdk\Unit\Extension\ValueResolver;
+
+use Codeception\Test\Unit;
+use SprykerSdk\Sdk\Core\Application\Dependency\InteractionProcessorInterface;
+use SprykerSdk\Sdk\Extension\Exception\UnresolvableValueExceptionException;
+use SprykerSdk\Sdk\Extension\ValueResolver\PriorityPathValueResolver;
+use SprykerSdk\SdkContracts\Entity\ContextInterface;
+
+class PriorityValueResolverTest extends Unit
+{
+    /**
+     * @var \SprykerSdk\Sdk\Core\Application\Dependency\InteractionProcessorInterface
+     */
+    protected InteractionProcessorInterface $valueReceiver;
+
+    /**
+     * @var \SprykerSdk\SdkContracts\Entity\ContextInterface
+     */
+    protected ContextInterface $context;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->valueReceiver = $this->createMock(InteractionProcessorInterface::class);
+
+        $this->context = $this->createMock(ContextInterface::class);
+
+        parent::setUp();
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetValue(): void
+    {
+        // Arrange
+        $this->valueReceiver
+            ->expects($this->once())
+            ->method('has')
+            ->willReturn(true);
+        $this->valueReceiver
+            ->expects($this->once())
+            ->method('get')
+            ->willReturn('./');
+        $valueResolver = new PriorityPathValueResolver($this->valueReceiver);
+        $valueResolver->configure(['name' => 'key', 'description' => '']);
+        // Act
+        $value = $valueResolver->getValue($this->context, ['defaultValue' => 'value']);
+
+        // Assert
+        $this->assertSame('./', $value);
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetValueException(): void
+    {
+        // Arrange
+        $this->valueReceiver
+            ->expects($this->once())
+            ->method('has')
+            ->willReturn(true);
+        $this->valueReceiver
+            ->expects($this->once())
+            ->method('get')
+            ->willReturn('./none');
+        $valueResolver = new PriorityPathValueResolver($this->valueReceiver);
+        $valueResolver->configure(['name' => 'key', 'description' => '']);
+
+        // Assert
+        $this->expectException(UnresolvableValueExceptionException::class);
+
+        // Act
+        $valueResolver->getValue($this->context, ['defaultValue' => './data/file.txt']);
+    }
+}
