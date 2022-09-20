7e8363c7ecfbaddcab17d482b8505537b0c81b63
vol4onok <alexander.veselov@spryker.com>
2022-09-20 10:47:50 +0300

    SDK-899: Added int factory, create fix for sniffer taskset

---
+++ src/Infrastructure/Service/ValueReceiver/QuestionFactory/IntQuestionFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Copyright © 2019-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerSdk\Sdk\Infrastructure\Service\ValueReceiver\QuestionFactory;
+
+use SprykerSdk\Sdk\Core\Application\Exception\MissingValueException;
+use SprykerSdk\Sdk\Core\Domain\Enum\ValueTypeEnum;
+use Symfony\Component\Console\Question\Question;
+
+class IntQuestionFactory extends StringQuestionFactory
+{
+    /**
+     * @param \Symfony\Component\Console\Question\Question $question
+     *
+     * @return \Symfony\Component\Console\Question\Question
+     */
+    protected function addValidator(Question $question): Question
+    {
+        $question->setValidator(function ($value) {
+            if ($value === '' || $value === null) {
+                throw new MissingValueException('Value is required.');
+            }
+
+            if (!is_numeric($value)) {
+                throw new MissingValueException('Value type should be integer.');
+            }
+
+            return $value;
+        });
+
+        return $question;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getType(): string
+    {
+        return ValueTypeEnum::TYPE_INT;
+    }
+}
