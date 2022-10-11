b166b713a743dcd8dc87a669d53e9434927c6e31
Kyrylo Meretskyi <kyrylo.meretskyi@spryker.com>
2022-10-11 18:05:53 +0300

    SDK-1003: fixed linters' errors

--- src/Presentation/Web/Controller/IndexController.php
+++ src/Presentation/Web/Controller/IndexController.php
@@ -1,16 +1,22 @@
 <?php
 
+/**
+ * Copyright © 2019-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
 namespace SprykerSdk\Sdk\Presentation\Web\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController
 {
     /**
      * @Route("/")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(): Response
     {
         return new Response('<h1>Hi! SDK is working on Web server.</h1>');
     }
--- src/Presentation/Web/SprykerSdkWebBundle.php
+++ src/Presentation/Web/SprykerSdkWebBundle.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Copyright © 2019-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
 namespace SprykerSdk\Sdk\Presentation\Web;
 
 use SprykerSdk\Sdk\Presentation\Web\DependencyInjection\SprykerSdkWebExtension;
