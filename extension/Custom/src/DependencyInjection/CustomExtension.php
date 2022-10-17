7a80528da7926cbd14e00e7313673d3c971144d2
Sergey Romankov <sergey.romankov@spryker.com>
2022-10-17 15:49:19 +0300

    SDK-992 remove unused NS

--- extension/Custom/src/DependencyInjection/CustomExtension.php
+++ extension/Custom/src/DependencyInjection/CustomExtension.php
@@ -10,7 +10,6 @@ namespace Custom\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class CustomExtension extends Extension
