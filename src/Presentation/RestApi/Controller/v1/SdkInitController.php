a1d512977d2d57591a330073c3e1c9ad6e8e54e6
pavlo.maksymov <pavlo.maksymov@spryker.com>
2022-10-24 20:47:20 +0200

    SDK-845: Passed request parameters to initializer.

--- src/Presentation/RestApi/Controller/v1/SdkInitController.php
+++ src/Presentation/RestApi/Controller/v1/SdkInitController.php
@@ -10,17 +10,19 @@ namespace SprykerSdk\Sdk\Presentation\RestApi\Controller\v1;
 use SprykerSdk\Sdk\Presentation\RestApi\Adapter\Initializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class SdkInitController extends AbstractController
 {
     /**
      * @param \SprykerSdk\Sdk\Presentation\RestApi\Adapter\Initializer $initializer
+     * @param \Symfony\Component\HttpFoundation\Request $request
      *
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function init(Initializer $initializer): JsonResponse
+    public function init(Initializer $initializer, Request $request): JsonResponse
     {
-        $initialized = $initializer->initialize([]);
+        $initialized = $initializer->initialize($request->query->all());
 
         return $this->json(['result' => $initialized ? 'Ok' : 'Failed']);
     }
