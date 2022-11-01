f6e8a37deb51228c4e016ee692e9263d0bc05655
Kyrylo Meretskyi <86324025+Kyrylo-Meretskyi@users.noreply.github.com>
2022-11-01 14:37:25 +0200

    SDK-1076: Create task to expose `spryk-run` (#259)
    * SDK-1076: Created Task to expose spryk-run

---
+++ tests/Sdk/Acceptance/Extension/Task/SprykRunTaskCest.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Copyright © 2019-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerSdk\Sdk\Acceptance\Extension\Task;
+
+use PHPUnit\Framework\Assert;
+use SprykerSdk\Sdk\Tests\AcceptanceTester;
+
+/**
+ * Auto-generated group annotations
+ *
+ * @group Sdk
+ * @group Acceptance
+ * @group Extension
+ * @group Task
+ * @group SprykRunTaskCest
+ * Add your own group annotations below this line
+ */
+class SprykRunTaskCest
+{
+    /**
+     * @var string
+     */
+    protected const COMMAND = 'spryk:run';
+
+    /**
+     * @param \SprykerSdk\Sdk\Tests\AcceptanceTester $I
+     *
+     * @return void
+     */
+    public function testSprykRunRunsSuccessfully(AcceptanceTester $I): void
+    {
+        // Arrange
+        $I->skipCliInteractiveTest();
+
+        $jsonArray = [
+            'Object' => [
+                'name' => [
+                    'key' => 'John',
+                ],
+            ],
+        ];
+        $expectedJson = json_encode($jsonArray);
+        $pathToJsonFile = $I->getPathFromProjectRoot('src/Pyz/Glue/AuthRestApi/Object.json');
+        $I->createFileWithContent($pathToJsonFile, $expectedJson);
+
+        // Act
+        $process = $I->runSdkCommand([
+            static::COMMAND,
+            '--spryk=UpdateJson',
+            '--targetModule=Pyz.AuthRestApi.Glue',
+            '--option=--targetFilename=Object.json',
+            '--option=--key=key',
+            '--option=--value=' . $jsonArray['Object']['name']['key'],
+            '--option=--targetPath=src/Pyz/Glue/AuthRestApi/',
+            '--option=--target=Object.name',
+            '--option=--no-interaction',
+            '--no-interaction',
+            '--quiet',
+        ]);
+
+        // Assert
+        Assert::assertTrue($process->isSuccessful());
+        Assert::assertJsonStringEqualsJsonString($expectedJson, file_get_contents($pathToJsonFile));
+        $I->removeFile($pathToJsonFile);
+    }
+}
