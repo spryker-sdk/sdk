5afd6019ee678aff8fd59a58058f3afb1423680c
vol4onok <alexander.veselov@spryker.com>
2022-11-03 15:39:37 +0200

    SDK-1125: Created project path for API

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 define('APPLICATION_ROOT_DIR', dirname(__DIR__, 1));
+chdir('/project');
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
