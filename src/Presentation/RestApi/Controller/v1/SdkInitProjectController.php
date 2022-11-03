f931461354429bd0dd9c207a9ee2e08ece816ad8
pavlo.maksymov <pavlo.maksymov@spryker.com>
2022-11-03 09:36:06 +0100

    SDK-866: Resolved merge conflicts.

--- src/Presentation/RestApi/Controller/v1/SdkInitProjectController.php
+++ src/Presentation/RestApi/Controller/v1/SdkInitProjectController.php
@@ -7,6 +7,7 @@
 
 namespace SprykerSdk\Sdk\Presentation\RestApi\Controller\v1;
 
+use OpenApi\Annotations as OA;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
