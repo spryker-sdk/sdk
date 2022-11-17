3c00df8b18315ee584b89b08e1e04bb166786720
Sergey Romankov <sergey.romankov@spryker.com>
2022-11-17 14:09:39 +0300

    SDK-1064 CR fixes

--- app/Migrations/Version20221115141126.php
+++ app/Migrations/Version20221115141126.php
@@ -19,7 +19,7 @@ final class Version20221115141126 extends AbstractMigration
         $this->addSql('CREATE TEMPORARY TABLE __temp__sdk_setting AS SELECT id, path, "values", strategy, type, has_initialization, initialization_description, initializer, setting_type FROM sdk_setting');
         $this->addSql('DROP TABLE sdk_setting');
         $this->addSql('CREATE TABLE sdk_setting (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, path VARCHAR(255) NOT NULL, "values" CLOB DEFAULT NULL --(DC2Type:json)
-        , strategy VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, has_initialization BOOLEAN NOT NULL, initialization_description VARCHAR(255) DEFAULT NULL, initializer VARCHAR(255) DEFAULT NULL, setting_type CHECK(setting_type IN (\'sdk\',\'local\',\'shared\')), force_ask_value BOOLEAN NOT NULL DEFAULT 0)');
+        , strategy VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, has_initialization BOOLEAN NOT NULL, initialization_description VARCHAR(255) DEFAULT NULL, initializer VARCHAR(255) DEFAULT NULL, setting_type VARCHAR(30) CHECK(setting_type IN (\'sdk\',\'local\',\'shared\')) NOT NULL, force_ask_value BOOLEAN NOT NULL DEFAULT 0)');
         $this->addSql('INSERT INTO sdk_setting (id, path, "values", strategy, type, has_initialization, initialization_description, initializer, setting_type) SELECT id, path, "values", strategy, type, has_initialization, initialization_description, initializer, setting_type FROM __temp__sdk_setting');
         $this->addSql('DROP TABLE __temp__sdk_setting');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8463B8E1B548B0F ON sdk_setting (path)');
