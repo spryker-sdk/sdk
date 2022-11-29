61b195add11f8477b456b805bf9dba31b257f53a
vol4onok <alexander.veselov@spryker.com>
2022-11-29 14:51:35 +0200

    SDK-1303: Fixed local pathes

--- bin/spryker-sdk.sh
+++ bin/spryker-sdk.sh
@@ -5,10 +5,10 @@ if ! docker info > /dev/null 2>&1; then
     exit 1
 fi
 
-SDK_DIR="$(dirname $(dirname $0))"
+LOCAL_SDK_DIR="$(realpath $(dirname $(dirname $0)))"
 
 if [[ $# == 1 && ($@ == "--version" || $@ == "-V") ]]; then
-    cat $SDK_DIR/VERSION
+    cat $LOCAL_SDK_DIR/VERSION
     exit 0
 fi
 
@@ -40,10 +40,10 @@ fi
 
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
-export EXECUTABLE_FILE_PATH="$(cd "$(dirname "$0")";pwd)/spryker-sdk.sh"
 export USER_UID="$(id -u)"
-export SDK_DIR="$SDK_DIR"
-export SDK_VERSION="$(cat "$SDK_DIR/VERSION")"
+export LOCAL_SDK_DIR="$LOCAL_SDK_DIR"
+export EXECUTABLE_FILE_PATH="$(cd "$(dirname "$0")";pwd)/spryker-sdk.sh"
+export SDK_VERSION="$(cat "$LOCAL_SDK_DIR/VERSION")"
 export UNAME_INFO="$(uname -a)"
 
 ### rest api server
@@ -66,26 +66,27 @@ rest-api-start*)
     export SPRYKER_XDEBUG_HOST_IP=${myIp}
     export PHP_IDE_CONFIG=serverName=spryker-sdk
     export SDK_XDEBUG_MODE="$ARG_XDEBUG_MODE"
-    docker-compose -f "${SDK_DIR}/docker-compose.rest-api.dev.yaml" up -d
+
+    docker-compose -f "${LOCAL_SDK_DIR}/docker-compose.rest-api.dev.yaml" up -d
     exit 0
     ;;
 "rest-api-stop")
-    docker-compose -f "${SDK_DIR}/docker-compose.rest-api.dev.yaml" stop
+    docker-compose -f "${LOCAL_SDK_DIR}/docker-compose.rest-api.dev.yaml" stop
     exit 0
     ;;
 "rest-api-status")
-    docker-compose -f "${SDK_DIR}/docker-compose.rest-api.dev.yaml" ps
+    docker-compose -f "${LOCAL_SDK_DIR}/docker-compose.rest-api.dev.yaml" ps
     exit 0
     ;;
 
 "rest-api-rm")
-    docker-compose -f "${SDK_DIR}/docker-compose.rest-api.dev.yaml" rm -s
+    docker-compose -f "${LOCAL_SDK_DIR}/docker-compose.rest-api.dev.yaml" rm -s
     exit 0
     ;;
 esac
 
 install_composer () {
-    if [[ ! -d "$SDK_DIR/vendor" ]]; then
+    if [[ ! -d "LOCAL_SDK_DIR/vendor" ]]; then
        composer install --no-scripts --no-interaction --optimize-autoloader --ignore-platform-reqs;
     fi;
 }
@@ -99,7 +100,7 @@ case $MODE in
     fi
     echo "Ensure mutagen is running by executing: mutagen compose -f docker-compose.yml -f docker-compose.dev.yml up -d"
     install_composer
-    docker-compose -f "${SDK_DIR}/docker-compose.dev.yml" run --rm \
+    docker-compose -f "${LOCAL_SDK_DIR}/docker-compose.dev.yml" run --rm \
       -e SPRYKER_XDEBUG_HOST_IP="${myIp}" \
       -e PHP_IDE_CONFIG="serverName=spryker-sdk" \
       spryker-sdk "$ARGUMENTS"
@@ -109,18 +110,18 @@ case $MODE in
         ARGUMENTS='/bin/bash'
     fi
     if [[ -z "$SPRYKER_SDK_ENV" || $SPRYKER_SDK_ENV == 'prod' ]]; then
-        docker-compose -f "${SDK_DIR}/docker-compose.yml" run --entrypoint="/bin/bash -c" --rm spryker-sdk "$ARGUMENTS"
+        docker-compose -f "${LOCAL_SDK_DIR}/docker-compose.yml" run --entrypoint="/bin/bash -c" --rm spryker-sdk "$ARGUMENTS"
     else
         install_composer
-        docker-compose -f "${SDK_DIR}/docker-compose.dev.yml" run --entrypoint="/bin/bash -c" --rm -e XDEBUG_MODE=off -w /data spryker-sdk "$ARGUMENTS"
+        docker-compose -f "${LOCAL_SDK_DIR}/docker-compose.dev.yml" run --entrypoint="/bin/bash -c" --rm -e XDEBUG_MODE=off -w /data spryker-sdk "$ARGUMENTS"
     fi
     ;;
 *)
     if [[ -z "$SPRYKER_SDK_ENV" || $SPRYKER_SDK_ENV == 'prod' ]]; then
-        docker-compose -f "${SDK_DIR}/docker-compose.yml" run --rm spryker-sdk "$ARGUMENTS"
+        docker-compose -f "${LOCAL_SDK_DIR}/docker-compose.yml" run --rm spryker-sdk "$ARGUMENTS"
     else
         install_composer
-        docker-compose -f "${SDK_DIR}/docker-compose.dev.yml" run --rm -e XDEBUG_MODE=off spryker-sdk "$ARGUMENTS"
+        docker-compose -f "${LOCAL_SDK_DIR}/docker-compose.dev.yml" run --rm -e XDEBUG_MODE=off spryker-sdk "$ARGUMENTS"
     fi
   ;;
 esac
