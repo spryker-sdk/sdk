02e2200717e38b77d64231dbd0c11a83d1ed4a1d
Pavel Maksimov <82393959+pavelmaksimov25@users.noreply.github.com>
2023-07-24 19:50:15 +0200

    SDK-2901 - Fixed tests.

--- tests/Sdk/Unit/Infrastructure/Violation/Formatter/ViolationReportDecoratorTest.php
+++ tests/Sdk/Unit/Infrastructure/Violation/Formatter/ViolationReportDecoratorTest.php
@@ -8,8 +8,6 @@
 namespace Sdk\Unit\Infrastructure\Violation\Formatter;
 
 use Codeception\Test\Unit;
-use CodeCompliance\Domain\Entity\PackageViolationReportInterface;
-use CodeCompliance\Domain\Entity\ViolationInterface;
 use SprykerSdk\Sdk\Infrastructure\Violation\Formatter\OutputViolationDecoratorInterface;
 use SprykerSdk\Sdk\Infrastructure\Violation\Formatter\ViolationReportDecorator;
 use SprykerSdk\SdkContracts\Report\Violation\ViolationReportInterface;
@@ -46,30 +44,10 @@ class ViolationReportDecoratorTest extends Unit
     public function testDecorate(): void
     {
         // Arrange
-        $violationMock = $this->createMock(ViolationInterface::class);
         $violationReportMock = $this->createMock(ViolationReportInterface::class);
-        $packageViolationReportMock = $this->createMock(PackageViolationReportInterface::class);
-        $packageViolationReportMock
-            ->expects($this->once())
-            ->method('getViolations')
-            ->willReturn([$violationMock]);
-
-        $packageViolationReportMock
-            ->expects($this->once())
-            ->method('getFileViolations')
-            ->willReturn(['path' => [$violationMock]]);
-
-        $violationReportMock
-            ->method('getViolations')
-            ->willReturn([$violationMock]);
-
-        $violationReportMock
-            ->expects($this->once())
-            ->method('getPackages')
-            ->willReturn([$packageViolationReportMock]);
 
         $this->outputViolationDecorator
-            ->expects($this->exactly(3))
+            ->expects($this->never())
             ->method('decorate')
             ->willReturnArgument(0);
 
